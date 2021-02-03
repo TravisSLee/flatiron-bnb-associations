41cbb8e4aa9d8b44579e94ed7a34ebcf9b1dfe64
TravisSLee <travisslee223@gmail.com>
2021-02-03 14:39:15 -0500

    commit tree 316e9bf492bede825c8913bbe3a5b141829938d4

---
+++ db/migrate/20210203185410_create_neighborhood.rb
@@ -0,0 +1,9 @@
+class CreateNeighborhood < ActiveRecord::Migration[5.0]
+  def change
+    create_table :neighborhoods do |t|
+      t.string :name
+      t.integer :city_id
+      t.integer :listing_id
+    end
+  end
+end
